50ac52dbac47fdef02e0b16f24ee46f2febc2f6e
Abrahan07 <161027641+Abrahan07@users.noreply.github.com>
2024-02-23 07:55:30 -0500

    Add files via upload

---
+++ EJERCICIOS_1/src/GIU/N_10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GIU;
+import java.util.Scanner;
+
+//Un alumno desea saber cual será su calificación final en la
+//materia de Algoritmos. Dicha calificación se compone de los
+//siguientes porcentajes:
+//55% del promedio de sus tres calificaciones parciales.
+//30% de la calificación del examen final.
+//15% de la calificación de un trabajo final.
+
+public class N_10 {
+    public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+        
+        
+    }
+    
+}
